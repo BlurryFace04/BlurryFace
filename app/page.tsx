cebec5a11e879f3de7ba8f6663234a493a72c6f1
BlurryFace04 <arnav29104@gmail.com>
2023-06-12 00:30:13 +0530

    personalized

--- app/page.tsx
+++ app/page.tsx
@@ -10,34 +10,43 @@ const navigation = [
 export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-			<nav className="my-16 animate-fade-in">
-				<ul className="flex items-center justify-center gap-4">
-					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-base duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
-					))}
-				</ul>
-			</nav>
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+			<div className="my-16 flex justify-between items-center space-x-4 w-11/12 mx-auto">
+				<div className="flex flex-col items-center justify-center w-1/12">
+				</div>
+				<div className="flex flex-col items-center justify-center w-3/5">
+					<nav className="mb-4">
+						<ul className="flex items-center justify-center gap-4">
+							{navigation.map((item) => (
+								<Link
+									key={item.href}
+									href={item.href}
+									className="text-xl duration-500 text-zinc-400 hover:text-zinc-300"
+								>
+									{item.name}
+								</Link>
+							))}
+						</ul>
+					</nav>
+					<h1 className="pb-4 font-extrabold tracking-tight text-transparent text-7xl lg:text-8xl bg-clip-text bg-gradient-to-r text-zinc-200 aos-init aos-animate">
+						Arnav Jain
+					</h1>
+					<h2 className="text-3xl font-bold text-zinc-300 mt-2">
+						Blockchain Developer
+					</h2>
+					<div className="w-2/3 mx-auto">
+						<h2 className="mb-8 text-lg text-zinc-400 aos-init aos-animate mt-10 text-justify">
+							Hey there! I'm Arnav, a self-taught blockchain developer and a proud college dropout. I started my journey in computer science at an university, but soon realized that I learn best when I'm working on real-world projects and teaching myself. So, I took the plunge and decided to focus full-time on what I love: diving into all sorts of tech challenges, from blockchain to just about anything in computer science. Take a look around my site to see the cool stuff I've been working on!
+						</h2>
+					</div>
+				</div>
+				<div className="w-1/5 ml-auto">
+					<img src="/Blurry.png" alt="Arnav Jain" className="w-full h-auto" />
+				</div>
+			</div>
 			<Particles
-				className="absolute inset-0 -z-10 animate-fade-in"
+				className="absolute inset-0 -z-10"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				Arnav Jain
-			</h1>
-
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-base text-zinc-500 ">
-					Hey there! I'm Arnav, a self-taught blockchain developer and a proud college dropout. I started my journey in computer science at symbiosis university of applied sciences, but soon realized that I learn best when I'm working on real-world projects and teaching myself. So, I took the plunge and decided to focus full-time on what I love: diving into all sorts of tech challenges, from blockchain to just about anything in computer science. Take a look around my site to see the cool stuff I've been working on!
-				</h2>
-			</div>
 		</div>
 	);
 }
